2885599eae1c995a3848862208a9e9ce5a4c1d28
core <seorisachs@gmail.com>
2018-11-19 16:10:05 -0500

    Sheep movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+  public float speed;
+
+  Vector3 forward = new Vector3(0f, 0f, -10f);
+  Vector3 backwards = new Vector3(-10f, 0f, 10f);
+  Vector3 right = new Vector3(0f, 0f, 10f);
+  Vector3 left = new Vector3(10f, 0f, 10f);
+
+
+
+
+  // Use this for initialization
+  void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    //if(Input.GetKey(KeyCode.W)){
+    //  transform.position += forward * Time.deltaTime;
+
+    //  transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+    //}
+
+    //if(Input.GetKey(KeyCode.S)){
+    //  transform.position += backwards * Time.deltaTime;
+    //}
+
+    //if (Input.GetKey(KeyCode.A))
+    //{
+    //  transform.position += left * Time.deltaTime;
+    //}
+
+    //if (Input.GetKey(KeyCode.D))
+    //{
+    //  transform.position += right * Time.deltaTime;
+    //}
+
+    SheepMoveForward();
+
+
+
+  }
+
+  void SheepMoveForward(){
+
+    //while(Time.time < 2){
+
+
+      transform.position += forward * Time.deltaTime;
+      
+   // }
+
+  }
+}
---
+++ Assets/Scripts/SheepWalk.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepWalk : MonoBehaviour {
+
+  public Animator anim;
+  int walkHash = Animator.StringToHash("Walk");
+  int walkStateHash = Animator.StringToHash("New Layer.Walk");
+ 
+
+	// Use this for initialization
+	void Start () {
+    print(walkStateHash);
+
+
+    anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    float move = Input.GetAxis("Horizontal");
+    anim.SetFloat("Speed", move);
+
+   // AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+    if(Input.GetKeyDown(KeyCode.W)){
+      print("I'm walking here! ");
+      anim.SetTrigger(walkHash);
+
+    }
+		
+	}
+}
