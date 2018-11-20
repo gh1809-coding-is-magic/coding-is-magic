508b0e542b3139d44adfb59fcd3150c9d98131e5
Hannah Barton <hannbarton@gmail.com>
2018-11-19 20:32:42 -0500

    sheep is walking and animating

--- Assets/Scripts/SheepWalk.cs
+++ Assets/Scripts/SheepWalk.cs
@@ -2,34 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SheepWalk : MonoBehaviour {
+public class SheepWalk : MonoBehaviour
+{
+    static Animator anim;
+    public float speed = 10f;
+    public float rotationSpeed = 75.0f;
+    public float duration = 1f;
+    float elapsedTime = Mathf.Infinity;
 
-  public Animator anim;
-  int walkHash = Animator.StringToHash("Walk");
-  int walkStateHash = Animator.StringToHash("New Layer.Walk");
- 
+    Vector3 forward = new Vector3(0f, 0f, -10f);
+    Vector3 backwards = new Vector3(-10f, 0f, 10f);
+    Vector3 right = new Vector3(0f, 0f, 10f);
+    Vector3 left = new Vector3(10f, 0f, 10f);
 
-	// Use this for initialization
-	void Start () {
-    print(walkStateHash);
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 
-    anim = GetComponent<Animator>();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
 
-    float move = Input.GetAxis("Horizontal");
-    anim.SetFloat("Speed", move);
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
 
-   // AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
-    if(Input.GetKeyDown(KeyCode.W)){
-      print("I'm walking here! ");
-      anim.SetTrigger(walkHash);
+        if (translation != 0 || rotation != 0)
+        {
+            anim.SetBool("isWalking", false);
+        }
+        else
+        {
+            anim.SetBool("isWalking", true);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(SheepMoveForward());
+
+        }
+    }
+    IEnumerator SheepMoveForward()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            anim.SetBool("isWalking", false);
+            yield return null;
+        }
 
     }
-		
-	}
 }
