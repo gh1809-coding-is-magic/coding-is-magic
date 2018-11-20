00f0c68b444772eded95a78ec59b6de1104157a0
core <seorisachs@gmail.com>
2018-11-20 18:30:23 -0500

    instructions

---
+++ Assets/Scripts/SheepWalk.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepWalk : MonoBehaviour
+{
+    static Animator anim;
+    public float speed = 10f;
+    public float rotationSpeed = 75.0f;
+    public float duration = 1f;
+    float elapsedTime = Mathf.Infinity;
+
+    Vector3 forward = new Vector3(0f, 0f, -10f);
+    // Vector3 backwards = new Vector3(-10f, 0f, 10f);
+    // Vector3 right = new Vector3(0f, 0f, 10f);
+    Vector3 left = new Vector3(10f, 0f, 10f);
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (translation != 0 || rotation != 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            TurnNinety();
+        }
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            StartCoroutine(SheepMoveForward());
+        }
+    }
+
+    void BlockyMove() {
+        StartCoroutine(SheepMoveForward());
+    }
+    IEnumerator SheepMoveForward()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            anim.SetBool("isWalking", true);
+            yield return null;
+        }
+
+    }
+    void TurnNinety() {
+        transform.Rotate(0, 90, 0);
+        anim.SetBool("isWalking", true);
+    }
+    void WalkBridge() {
+        transform.Translate(0, 0, 10f);
+        anim.SetBool("isWalking", true);
+    }
+}
--- client/components/BlocklyWorkspace.js
+++ client/components/BlocklyWorkspace.js
@@ -1,80 +1,73 @@
-import React from 'react';
-import Blockly from 'node-blockly/browser';
-import BlocklyDrawer, {Block, Category, workspaceXML} from 'react-blockly-drawer';
+import React from 'react'
+import Blockly from 'node-blockly/browser'
+import BlocklyDrawer, {
+  Block,
+  Category,
+  workspaceXML
+} from 'react-blockly-drawer'
+import spellConstructor, {move1} from './spellConstructor'
 
 class BlocklyWorkspace extends React.Component {
   constructor() {
-    super();
+    super()
 
     //State keeps track of outputted Unity messages and blocks in the workspace
     this.state = {
       currCode: '',
       currWorkspace: ''
-    };
+    }
 
     //Move block 1 step definition
     //*Should probably refactor this and place definition outside of constructor
-    this.helloWorld = {
-      name: 'Move 1 Step',
-      category: 'Movement',
-      block: {
-        init: function() {
-          this.jsonInit({
-            message0: 'Move 1 step',
-            output: 'String',
-            colour: 160,
-            tooltip: 'Moves the object one step',
-          })
-        }
-      },
-      generator: block => {
-        const code = `this.props.unitySendMessage('Sheep_Demo', 'BlockyMove')`;
-        return [code, Blockly.JavaScript.ORDER_NONE]
-      }
-    };
+    this.helloWorld = move1()
 
-    this.runCode = this.runCode.bind(this);
-  };
+    this.runCode = this.runCode.bind(this)
+  }
 
   //Evaluates code on submit and sends message to Unity
   runCode() {
-    eval(this.state.currCode);
-  };
+    console.log('State of current code: ', this.state.currCode)
+    console.log('Type: ', typeof this.state.currCode)
+    eval(this.state.currCode)
+  }
 
   //False to prevent placed blocks from disappearing with each new state change
   shouldComponentUpdate() {
-    return false;
-  };
+    return false
+  }
 
   //Sets up Blockly workspace and updates state with each new block placement
   render() {
+    console.log('Move1: ', move1)
+    console.log('Hello world: ', this.helloWorld)
     return (
       <div id="blockly-content">
         <BlocklyDrawer
-        tools={[this.helloWorld]}
-        onChange={(code, workspace) => {
-          this.setState({currCode: code, currWorkspace: workspace})
-        }}
-        appearance={
-          {
+          tools={[this.helloWorld]}
+          onChange={(code, workspace) => {
+            console.log('CHANGING THE CODE: ', code)
+            this.setState({currCode: code, currWorkspace: workspace})
+          }}
+          appearance={{
             categories: {
               Demo: {
                 colour: '270'
-              },
-            },
-          }
-        }
-      >
-        <Category name="Variables" custom="VARIABLE" />
-        <Category name="Values">
-          <Block type="math_number" />
-          <Block type="text" />
-        </Category>
-      </BlocklyDrawer>
-      <button type="button" onClick={()=>this.runCode()}>Run</button>
-    </div>
+              }
+            }
+          }}
+        >
+          <Category name="Variables" custom="VARIABLE" />
+          <Category name="Values">
+            <Block type="math_number" />
+            <Block type="text" />
+          </Category>
+        </BlocklyDrawer>
+        <button type="button" onClick={() => this.runCode()}>
+          Run
+        </button>
+      </div>
     )
-  };
-};
+  }
+}
 
-export default BlocklyWorkspace;
+export default BlocklyWorkspace
