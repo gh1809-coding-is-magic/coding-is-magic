f08c7ac518dcab17020fa05903ead63c36860d85
Hannah Barton <hannbarton@gmail.com>
2018-11-20 18:15:21 -0500

    camera follow

--- Assets/Scripts/SheepWalk.cs
+++ Assets/Scripts/SheepWalk.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class SheepWalk : MonoBehaviour
 {
     static Animator anim;
-    public float speed = 10f;
+    public float speed = 50f;
     public float rotationSpeed = 75.0f;
     public float duration = 1f;
     float elapsedTime = Mathf.Infinity;
 
     Vector3 forward = new Vector3(0f, 0f, -10f);
     // Vector3 backwards = new Vector3(-10f, 0f, 10f);
-    // Vector3 right = new Vector3(0f, 0f, 10f);
-    Vector3 left = new Vector3(10f, 0f, 10f);
+    Vector3 right = new Vector3(0f, 0f, 10f);
+    // Vector3 left = new Vector3(10f, 0f, 10f);
     private Vector3 moveDirection = Vector3.zero;
 
     // Use this for initialization
@@ -59,7 +59,7 @@ public class SheepWalk : MonoBehaviour
         float elapsedTime = 0f;
         while (elapsedTime < duration)
         {
-            transform.Translate(Vector3.forward * Time.deltaTime);
+            transform.Translate(Vector3.forward * Time.deltaTime * 10f);
             elapsedTime += Time.deltaTime;
             anim.SetBool("isWalking", true);
             yield return null;
@@ -68,10 +68,6 @@ public class SheepWalk : MonoBehaviour
     }
     void TurnNinety() {
         transform.Rotate(0, 90, 0);
-        anim.SetBool("isWalking", true);
-    }
-    void WalkBridge() {
-        transform.Translate(0, 0, 10f);
         anim.SetBool("isWalking", true);
     }
 }
---
+++ Assets/Scripts/introText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class introText : MonoBehaviour {
+
+  public Text textShown = null;
+  public void NextWord (string word){
+
+    textShown.text = word;
+  }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
