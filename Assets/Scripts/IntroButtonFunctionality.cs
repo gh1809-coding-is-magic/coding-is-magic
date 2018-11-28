b8465d4d06b5f6e76c6bb2ebc4a610338a409123
core <seorisachs@gmail.com>
2018-11-28 13:26:00 -0500

    Continue button greys out after instructions are done. Color changes.

--- Assets/Scripts/IntroButtonFunctionality.cs
+++ Assets/Scripts/IntroButtonFunctionality.cs
@@ -5,34 +5,35 @@ using UnityEngine.UI;
 
 public class IntroButtonFunctionality : MonoBehaviour
 {
-
   public Button continueGame;
   float counter = 0f;
 
   // Use this for initialization
   void Start()
   {
+    continueGame.onClick.AddListener(TaskOnClick);
 
+  }
+
+  void TaskOnClick()
+  {
+    Debug.Log("great!");
+
+    counter++;
+
+    Debug.Log(counter);
+
+    if (counter == 6)
+    {
+
+      continueGame.interactable = false;
+    }
   }
 
   // Update is called once per frame
   void Update()
   {
 
-    if (Input.GetMouseButtonDown(0))
-    {
-      Debug.Log("please");
-
-      counter++;
-
-      Debug.Log(counter);
-    }
-
-    if (counter == 7)
-    {
-
-      continueGame.interactable = false;
-    }
   }
 
   void OnMouseDown()
