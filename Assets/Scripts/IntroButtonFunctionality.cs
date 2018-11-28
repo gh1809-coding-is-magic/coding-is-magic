2041bc2789ce2983f35a4bad59cb741cf989643e
Hannah Barton <hannbarton@gmail.com>
2018-11-28 16:19:10 -0500

    working files revert back to previous state

---
+++ Assets/Scripts/IntroButtonFunctionality.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroButtonFunctionality : MonoBehaviour
+{
+  public Button continueGame;
+  float counter = 0f;
+
+  // Use this for initialization
+  void Start()
+  {
+    continueGame.onClick.AddListener(TaskOnClick);
+
+  }
+
+  void TaskOnClick()
+  {
+    Debug.Log("great!");
+
+    counter++;
+
+    Debug.Log(counter);
+
+    if (counter == 6)
+    {
+
+      continueGame.interactable = false;
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void OnMouseDown()
+  {
+    Debug.Log("clicked!");
+  }
+}
