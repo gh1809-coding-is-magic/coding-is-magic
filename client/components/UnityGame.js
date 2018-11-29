74d08643efffc078d5b9b06ac9ff812a46430b8c
Hannah Barton <hannbarton@gmail.com>
2018-11-29 12:29:01 -0500

    added cass pic

--- client/components/UnityGame.js
+++ client/components/UnityGame.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Unity, {UnityContent} from 'react-unity-webgl'
 import BlocklyWorkspace from './BlocklyWorkspace'
+import Main from './Main'
 
 class UnityGame extends React.Component {
   constructor() {
@@ -22,18 +23,19 @@ class UnityGame extends React.Component {
   render() {
     return (
       <div>
+        <Main />
         <div id="mobile-message">
           Not mobile or tablet compatible. Please view on a desktop!
         </div>
-        <div id="browser-container">
+        {/* <div id="browser-container">
           <div id="inner-unity">
             <Unity unityContent={this.unityContent} />
             {/* <Unity src="Build/Build/Build.json" loader="Build/Build/UnityLoader.js" /> */}
-          </div>
-          <div id="inner-blockly">
-            <BlocklyWorkspace unitySendMessage={this.unitySendMessage} />
-          </div>
-        </div>
+          {/* </div> */}
+           {/* <div id="inner-blockly">
+             <BlocklyWorkspace unitySendMessage={this.unitySendMessage} />
+           </div>
+         </div> */}
       </div>
     )
   }
